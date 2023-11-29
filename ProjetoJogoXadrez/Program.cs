5ff29a0518cc8193a7175947d180fb7f4d9ef96a
Bruno Silva <bruno.s.iwamura@gmail.com>
2023-11-29 16:19:32 -0300

    Implementando primeiras exceptions

--- ProjetoJogoXadrez/Program.cs
+++ ProjetoJogoXadrez/Program.cs
@@ -11,14 +11,20 @@ namespace ProjetoJogoXadrez
             Tabuleiro tab = new Tabuleiro (8,8);
 
             //Posicao posInit = new Posicao(0,0);
+            try
+            {
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Azul), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(3, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Azul), new Posicao(0, 0));
 
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0,0));
-            tab.colocarPeca(new Torre(tab, Cor.Azul), new Posicao(1,3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(3,4));
+                Tela.imprimirTabuleiro(tab);
 
-            Tela.imprimirTabuleiro (tab);
+                Console.ReadLine();
 
-            Console.ReadLine();
+            } catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
 
         }
     }
